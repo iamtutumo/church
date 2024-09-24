569584f11d683e9edc982b4ed3b4801db11bd777
Tutu Moses <etu.moses@gmail.com>
2024-09-24 23:10:50 +0300

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,7 +1,15 @@
 from setuptools import setup, find_packages
+import os
 
-with open("requirements.txt") as f:
-    install_requires = f.read().strip().split("\n")
+# Get the directory containing this file
+HERE = os.path.abspath(os.path.dirname(__file__))
+
+# Read the contents of requirements.txt file
+try:
+    with open(os.path.join(HERE, 'requirements.txt'), 'r') as f:
+        install_requires = f.read().strip().split("\n")
+except FileNotFoundError:
+    install_requires = []
 
 # get version from __version__ variable in church/__init__.py
 from church import __version__ as version
@@ -10,8 +18,8 @@ setup(
     name="church",
     version=version,
     description="A comprehensive church management system",
-    author="Tutu Moses",
-    author_email="etu.moses@gmail.com",
+    author="Your Name",
+    author_email="your.email@example.com",
     packages=find_packages(),
     zip_safe=False,
     include_package_data=True,
